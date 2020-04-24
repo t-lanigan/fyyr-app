dac9db2f6b76251bd8a71c7b6c0759b655a50252
Tyler Lanigan <tyler.lanigan@hootsuite.com>
2020-04-24 13:49:18 -0700

    flask db migrate

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgres://tyler.lanigan@localhost:5432/fyyr'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
