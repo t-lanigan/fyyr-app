8f4a1ce0d635f0e8a8a95f9fa7857f0596f6ee01
Tyler Lanigan <tyler.lanigan@hootsuite.com>
2020-04-28 10:00:57 -0700

    artist edit page done

--- app.py
+++ app.py
@@ -275,6 +275,7 @@ def artist_show_data_helper(shows):
 #  ----------------------------------------------------------------
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
+    # TODO: add button to edit on venue page
     form = ArtistForm()
     artist = Artist.query.filter_by(id=artist_id).first()
 
@@ -283,8 +284,28 @@ def edit_artist(artist_id):
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-    # TODO: take values from the form submitted, and update existing
-    # artist record with ID <artist_id> using the new attributes
+    artist = Artist.query.get(artist_id)
+    form = ArtistForm()
+    try:
+        artist.name = form.name.data,
+        artist.city = form.city.data,
+        artist.state = form.state.data,
+        artist.phone = form.phone.data,
+        artist.image_link = form.image_link.data,
+        artist.genres = form.genres.data
+        artist.facebook_link = form.facebook_link.data,
+        artist.website = form.website.data,
+        artist.seeking_description = form.seeking_description.data,
+        artist.seeking_talent = form.seeking_venue.data
+        db.session.commit()
+        flash('Artist ' + request.form['name'] + ' was successfully updated.')
+    except Exception as e:
+        app.logger.error(e)
+        flash('Something went wrong. Artist ' +
+              request.form['name'] + ' could not be updated.')
+        db.session.rollback()
+    finally:
+        db.session.close()
 
     return redirect(url_for('show_artist', artist_id=artist_id))
 
